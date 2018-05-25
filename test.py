9d73a19848872fc037cffab8517b2c3b036b13e0
aha81595 <38971026+aha81595@users.noreply.github.com>
2018-05-25 14:59:59 +0800

    小修

--- test.py
+++ test.py
@@ -5,11 +5,11 @@ class Main(QMainWindow,example_ui.Ui_MainWindow):
     def __init__(self):
         super(self.__class__,self).__init__()
         self.setupUi(self)
-        self.pushButton.clicked.connect(self.send)
+        self.pushButton.clicked.connect(self.login)
         self.pushButton_2.clicked.connect(self.send)
         self.pushButton_2.setEnabled(False)
         self.lineEdit_2.setEnabled(False)
-    def send(self):
+    def login(self):
         text=self.lineEdit.text()
         self.textBrowser.append("Welcome to chat room! "+text)
         self.textBrowser.update()
@@ -20,6 +20,11 @@ class Main(QMainWindow,example_ui.Ui_MainWindow):
         self.lineEdit.setEnabled(False)
         self.pushButton_2.setEnabled(True)
         self.lineEdit_2.setEnabled(True)
+    def send(self):
+        text=self.lineEdit_2.text()
+        self.textBrowser.append(text)
+        self.textBrowser.update()
+        self.lineEdit_2.setText("")
 if __name__=="__main__":
     app=QApplication(sys.argv)
     MainWindow=Main()
