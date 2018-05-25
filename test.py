9f33ac0bfa9837268e76b14959a2b07c142d1a50
paulx904 <37829775+paulx904@users.noreply.github.com>
2018-05-25 14:52:06 +0800

    可輸入nickname，並print出welcome訊息

--- test.py
+++ test.py
@@ -1,29 +1,20 @@
 from PyQt5.QtWidgets import QMainWindow,QApplication
 import example_ui
 import sys
-
 class Main(QMainWindow,example_ui.Ui_MainWindow):
     def __init__(self):
         super(self.__class__,self).__init__()
         self.setupUi(self)
-        self.pushButton.setText("Login")
-        self.pushButton.clicked.connect(self.login)
-        self.pushButton_2.setText("send")
-        self.pushButton_2.clicked.connect(self.send)
-
-    def login(self):
-        name=self.lineEdit.text()
-        self.textBrowser.append(name)
+        self.pushButton.clicked.connect(self.send)
+    def send(self):
+        text=self.lineEdit.text()
+        self.textBrowser.append("Welcome to chat room! "+text)
+        self.textBrowser.update()
+        self.textBrowser.append("Now Lets Chat, "+text)
         self.textBrowser.update()
         self.lineEdit.setText("")
         self.pushButton.setEnabled(False)
         self.lineEdit.setEnabled(False)
-
-    def send(self):
-        text=self.lineEdit_2.text()
-        self.textBrowser.append(text)
-        self.textBrowser.update()
-        self.lineEdit_2.setText("")
 if __name__=="__main__":
     app=QApplication(sys.argv)
     MainWindow=Main()
