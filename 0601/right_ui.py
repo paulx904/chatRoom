23bc5e532223a842e2b85aab1373bfb50b8b5e8c
j0915117518 <32322913+j0915117518@users.noreply.github.com>
2018-06-01 15:23:10 +0800

    add few comments

--- 0601/right_ui.py
+++ 0601/right_ui.py
@@ -14,37 +14,37 @@ class Ui_loginButton(object):
         loginButton.resize(410, 679)
         self.centralwidget = QtWidgets.QWidget(loginButton)
         self.centralwidget.setObjectName("centralwidget")
-        self.chatroom_people = QtWidgets.QTextBrowser(self.centralwidget)
+        self.chatroom_people = QtWidgets.QTextBrowser(self.centralwidget) #output the number of people in chatroom 
         self.chatroom_people.setGeometry(QtCore.QRect(10, 10, 391, 41))
         self.chatroom_people.setObjectName("chatroom_people")
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(10, 70, 61, 16))
         self.label.setObjectName("label")
-        self.nickName = QtWidgets.QLineEdit(self.centralwidget)
+        self.nickName = QtWidgets.QLineEdit(self.centralwidget) #where to input nickname
         self.nickName.setGeometry(QtCore.QRect(70, 60, 121, 41))
         self.nickName.setObjectName("nickName")
-        self.password = QtWidgets.QLineEdit(self.centralwidget)
+        self.password = QtWidgets.QLineEdit(self.centralwidget) #where to input password
         self.password.setGeometry(QtCore.QRect(200, 60, 121, 41))
         self.password.setObjectName("password")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget) #button to login
         self.pushButton.setGeometry(QtCore.QRect(330, 70, 75, 23))
         self.pushButton.setObjectName("pushButton")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setGeometry(QtCore.QRect(10, 120, 101, 16))
         self.label_2.setObjectName("label_2")
-        self.password_change = QtWidgets.QLineEdit(self.centralwidget)
+        self.password_change = QtWidgets.QLineEdit(self.centralwidget) #where to input change of password
         self.password_change.setGeometry(QtCore.QRect(110, 110, 211, 41))
         self.password_change.setObjectName("password_change")
-        self.password_change_button = QtWidgets.QPushButton(self.centralwidget)
+        self.password_change_button = QtWidgets.QPushButton(self.centralwidget) #button to change password
         self.password_change_button.setGeometry(QtCore.QRect(330, 120, 75, 23))
         self.password_change_button.setObjectName("password_change_button")
-        self.chaty = QtWidgets.QTextBrowser(self.centralwidget)
+        self.chaty = QtWidgets.QTextBrowser(self.centralwidget) #where to output chatroom message
         self.chaty.setGeometry(QtCore.QRect(10, 160, 391, 381))
         self.chaty.setObjectName("chaty")
-        self.send_text = QtWidgets.QLineEdit(self.centralwidget)
+        self.send_text = QtWidgets.QLineEdit(self.centralwidget) #where to input texts
         self.send_text.setGeometry(QtCore.QRect(10, 550, 391, 51))
         self.send_text.setObjectName("send_text")
-        self.send_button = QtWidgets.QPushButton(self.centralwidget)
+        self.send_button = QtWidgets.QPushButton(self.centralwidget) #button to send text
         self.send_button.setGeometry(QtCore.QRect(160, 610, 75, 23))
         self.send_button.setObjectName("send_button")
         loginButton.setCentralWidget(self.centralwidget)
