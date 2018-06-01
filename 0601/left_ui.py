ee581c8708bd502b83ddd2515667c6200bd7e5f2
j0915117518 <32322913+j0915117518@users.noreply.github.com>
2018-06-01 15:28:04 +0800

    add few comments

--- 0601/left_ui.py
+++ 0601/left_ui.py
@@ -17,19 +17,19 @@ class Ui_MainWindow(object):
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(10, 10, 51, 16))
         self.label.setObjectName("label")
-        self.nickname = QtWidgets.QLineEdit(self.centralwidget)
+        self.nickname = QtWidgets.QLineEdit(self.centralwidget) #where to input nickname
         self.nickname.setGeometry(QtCore.QRect(60, 10, 121, 21))
         self.nickname.setObjectName("nickname")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setGeometry(QtCore.QRect(190, 10, 51, 16))
         self.label_2.setObjectName("label_2")
-        self.password = QtWidgets.QLineEdit(self.centralwidget)
+        self.password = QtWidgets.QLineEdit(self.centralwidget) #where to input password
         self.password.setGeometry(QtCore.QRect(240, 10, 131, 21))
         self.password.setObjectName("password")
-        self.add_button = QtWidgets.QPushButton(self.centralwidget)
+        self.add_button = QtWidgets.QPushButton(self.centralwidget) #button to add client
         self.add_button.setGeometry(QtCore.QRect(10, 40, 361, 31))
         self.add_button.setObjectName("add_button")
-        self.delete_button = QtWidgets.QPushButton(self.centralwidget)
+        self.delete_button = QtWidgets.QPushButton(self.centralwidget) #button to delete client
         self.delete_button.setGeometry(QtCore.QRect(10, 530, 361, 31))
         self.delete_button.setObjectName("delete_button")
         self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
@@ -37,7 +37,7 @@ class Ui_MainWindow(object):
         self.tableWidget.setObjectName("tableWidget")
         self.tableWidget.setColumnCount(0)
         self.tableWidget.setRowCount(0)
-        self.checkBox = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox = QtWidgets.QCheckBox(self.centralwidget) #checkBox : client list
         self.checkBox.setGeometry(QtCore.QRect(20, 90, 73, 16))
         self.checkBox.setObjectName("checkBox")
         MainWindow.setCentralWidget(self.centralwidget)
