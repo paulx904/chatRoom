510afc776ddecaead10962a6ffcacb632e0fe91b
fanglei0925 <33367949+fanglei0925@users.noreply.github.com>
2018-06-01 15:16:25 +0800

    修改了新的參數

--- 0601/Client.py
+++ 0601/Client.py
@@ -1,61 +1,67 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication
-import mainwindow_ui
+import right_ui
 import sys
 import socket
 import threading
 from PyQt5.QtCore import *
 
-class Client(QMainWindow, mainwindow_ui.Ui_MainWindow):
+class Client(QMainWindow, right_ui.Ui_loginButton):
     def __init__(self, host, port):
         super(self.__class__, self).__init__()
         self.setupUi(self)
-        self.pushButton.clicked.connect(self.sendThreadFunc)#pushButton是送出訊息的按鈕   pushButton_2是登入
+        self.send_button.clicked.connect(self.sendThreadFunc)#send_button是送出訊息的按鈕   pushButton是登入
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock = sock
         self.sock.connect((host, port))
         self.sock.send(b'1')
-        self.pushButton_2.clicked.connect(self.send)
-        self.pushButton.setEnabled(False)
-        self.lineEdit.setEnabled(False)#lineEdit是輸入訊息的欄位
+        self.pushButton.clicked.connect(self.send)
+        self.send_button.setEnabled(False)
+        self.send_text.setEnabled(False)#send_text是輸入訊息的欄位
 
     def sendThreadFunc(self):
         try:
-            myword = self.lineEdit.text()
+            myword = self.send_text.text()
             self.sock.send(myword.encode())
-            self.textBrowser.append(" ")
+            self.chaty.append(" ")
             myword = myword+" :You"
-            self.textBrowser.setAlignment(Qt.AlignRight)
-            self.textBrowser.append(myword)
-            self.textBrowser.update()
-            self.lineEdit.setText("")
+            self.chaty.setAlignment(Qt.AlignRight)
+            self.chaty.append(myword)
+            self.chaty.update()
+            self.send_text.setText("")
         except ConnectionAbortedError:
-            self.textBrowser.append('Server closed this connection!')
-            self.textBrowser.update()
+            self.chaty.append('Server closed this connection!')
+            self.chaty.update()
         except ConnectionResetError:
-            self.textBrowser.append('Server is closed!')
-            self.textBrowser.update()
+            self.chaty.append('Server is closed!')
+            self.chaty.update()
 
     def recvThreadFunc(self):
         while True:
             try:
                 otherword = self.sock.recv(1024) # socket.recv(recv_size)
-                self.textBrowser.append(otherword.decode())
-                self.textBrowser.setAlignment(Qt.AlignLeft)
-                self.textBrowser.update()
+                self.chaty.append(otherword.decode())
+                self.chaty.setAlignment(Qt.AlignLeft)
+                self.chaty.update()
             except ConnectionAbortedError:
-                self.textBrowser.append('Server closed this connection!')
+                self.chaty.append('Server closed this connection!')
             except ConnectionResetError:
-                self.textBrowser.append('Server is closed!')
+                self.chaty.append('Server is closed!')
 
 
     def send(self):#11
-        name = self.lineEdit_2.text()
+        name = self.nickName.text()#nickname是暱稱
         self.sock.send(name.encode())
-        self.textBrowser.update()
-        self.lineEdit_2.setText("")
+        self.chaty.update()
+        self.nickName.setText("")
+        self.send_text.setEnabled(True)
+        self.send_button.setEnabled(True)
+        self.pushButton.setEnabled(False)
+        self.nickName.setEnabled(False)
+        self.password.setEnabled(False)
         th2 = threading.Thread(target=self.recvThreadFunc)
         th2.setDaemon(True)
         th2.start()
+
 
 def main():
     c = Client('140.138.145.20', 8000)
